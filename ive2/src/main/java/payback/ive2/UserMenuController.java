5a0869f5483aa94f5e4a360888163f85d8d1ee6e
GithubWoon <eo2458@naver.com>
2023-10-27 17:50:42 +0900

    1010포트

--- ive2/src/main/java/payback/ive2/UserMenuController.java
+++ ive2/src/main/java/payback/ive2/UserMenuController.java
@@ -1,14 +1,14 @@
 package payback.ive2;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import org.springframework.ui.Model;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+
 
 @Controller
 public class UserMenuController {
