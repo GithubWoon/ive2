1aac24b4467e33d7134f770c3ebe8907cf1fe8f7
GithubWoon <eo2458@naver.com>
2023-11-07 12:22:40 +0900

    ㅇㅅㅇ

--- ive2/src/main/java/payback/ive2/UserMenuController.java
+++ ive2/src/main/java/payback/ive2/UserMenuController.java
@@ -122,6 +122,7 @@ public class UserMenuController {
         String userId = (String) session.getAttribute("userId");
         Enumeration<String> parameterNames = request.getParameterNames();
 
+        // HashMap 에 메세지를 담을건가보다
         Map<String, String> response = new HashMap<>();
 
         while (parameterNames.hasMoreElements()) {
@@ -156,6 +157,67 @@ public class UserMenuController {
 
         response.put("message", "추가완료 한번 확인해봐");
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/look")
+    public ResponseEntity<Map<String, String>> handleLookRequest(@RequestParam String action, Model model,
+            HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        String userId = (String) session.getAttribute("userId");
+        String userName = (String) session.getAttribute("userName");
+
+        Map<String, String> response = new HashMap<>();
+
+        // 취소 클릭시
+        if (action.equals("취소")) {
+            // 로그인한 사용자의 BASKET 테이블의 모든 데이터 삭제
+            String sql = "DELETE FROM BASKET WHERE ID = ?";
+            jdbcTemplate.update(sql, userId);
+
+            response.put("message", "장바구니를 초기화하였습니다.");
+            return ResponseEntity.ok(response);
+        }
+
+        // 구매 클릭시
+        else if (action.equals("구매")) {
+            // 로그인한 사용자의 BASKET 테이블의 모든 데이터를 MENU 테이블에서 감소
+            String sqlSelect = "SELECT PRODUCTNAME, QUANTITY FROM BASKET WHERE ID = ?";
+            List<Map<String, Object>> basketItems = jdbcTemplate.queryForList(sqlSelect, userId);
+
+            // 현재 시간을 가져옴
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+            // RECEIPT 테이블에서 현재 PURCHASENUM의 최댓값을 찾음
+            String sqlMaxPurchaseNum = "SELECT MAX(PURCHASENUM) FROM RECEIPT";
+            Integer maxPurchaseNum = jdbcTemplate.queryForObject(sqlMaxPurchaseNum, Integer.class);
+
+            // 최댓값이 null인 경우(테이블에 데이터가 없는 경우) 처리
+            int purchaseNum = (maxPurchaseNum == null) ? 1 : maxPurchaseNum + 1;
+
+            for (Map<String, Object> item : basketItems) {
+                String productName = (String) item.get("PRODUCTNAME");
+                int quantity = ((BigDecimal) item.get("QUANTITY")).intValue();
+
+                // RECEIPT 테이블에 데이터 추가
+                String sqlInsert = "INSERT INTO RECEIPT (ID, NAME, PRODUCTNAME, QUANTITY, BUYDAY, PURCHASENUM) VALUES (?, ?, ?, ?, ?, ?)";
+                jdbcTemplate.update(sqlInsert, userId, userName, productName, quantity, timestamp, purchaseNum);
+
+                String sqlUpdate = "UPDATE MENU SET QUANTITY = QUANTITY - ? WHERE PRODUCTNAME = ?";
+                jdbcTemplate.update(sqlUpdate, quantity, productName);
+            }
+
+            // 로그인한 사용자의 BASKET 테이블의 모든 데이터 삭제
+            String sqlDelete = "DELETE FROM BASKET WHERE ID = ?";
+            jdbcTemplate.update(sqlDelete, userId);
+
+            response.put("message", "구매를 완료하였습니다.");
+            return ResponseEntity.ok(response);
+        }
+
+        else {
+            response.put("message", "알 수 없는 요청입니다.");
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 
 }
